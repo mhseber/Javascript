7d56664c8afe577f3cfe150a4ff35880bfb3b438
mahamudul hasan seber <sebermh@gmail.com>
2025-09-21 21:56:33 +0600

    Reboot My Js

---
+++ Callstack.js
@@ -0,0 +1,19 @@
+function first(){
+    second();
+    console.log("First");
+}
+
+function first(){
+    second();
+    console.log("second");
+}
+
+function first(){
+    second();
+    console.log("third");
+}
+
+function first(){
+    second();
+    console.log("global");
+}
---
+++ Reboot.js
@@ -0,0 +1,8 @@
+console.log("Reboot.js is running...");
+
+var a =5;
+function add (a,b){
+    return a + b;
+}
+const result =  add(2,3);
+console.log(result)
