6dffacfc2eb28dfea3f6e0581812262a5227a3dc
mahamudul hasan seber <sebermh@gmail.com>
2025-09-23 21:42:19 +0600

    ES6 Reboot

---
+++ ES6.js
@@ -0,0 +1,9 @@
+// const {name: studentName2, grade = "A+", address = "Dhaka"} = {} 
+
+
+
+const data = {profile: {email: "sebervi@gmail.com"}}
+
+const {profile : {email}} = data
+
+console.log(email)
